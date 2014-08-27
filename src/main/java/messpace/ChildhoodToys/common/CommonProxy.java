5ebf2f2f3e227d407d7880af878dacbc822b8d6f
carrotmcmuffinz <carrotmcmuffinz@gmail.com>
2014-08-26 19:27:41 -0500

    Redoing Upload
    Stuffs

--- src/main/java/messpace/ChildhoodToys/common/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package messpace.ChildhoodToys.common;
-
-public class CommonProxy {
-	
-	public void registerRenderers() {
-		
-	}
-
-}
--- src/main/java/messpace/ChildhoodToys/tileentity/TileEntityGlowStick.java
+++
@@ -1,7 +0,0 @@
-package messpace.ChildhoodToys.tileentity;
-
-import net.minecraft.tileentity.TileEntity;
-
-public class TileEntityGlowStick extends TileEntity{
-
-}
